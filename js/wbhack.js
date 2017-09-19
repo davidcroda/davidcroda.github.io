060f225586f5c1ada89af1949d531f34382bb95a
dermatobia <stansari@dramafever.com>
2017-09-19 09:05:32 -0700

    update data and chart layout

--- js/wbhack.js
+++ js/wbhack.js
@@ -2,12 +2,26 @@ var app = angular.module('myApp', ['chart.js', 'angularjs-gauge']);
 
 app.controller('MainController', function($scope) {
     var obj = {
-      labels: ['9/18', '9/20', '9/22', '9/24'],
+      labels: ['9/7', '9/14', '9/21', '9/28'],
       options: {
         elements: {
           line: {
             fill: false
           }
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Millions'
+            }
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Week'
+            }
+          }]
         }
       },
       datasetOverride: [
@@ -19,13 +33,13 @@ app.controller('MainController', function($scope) {
       ]
     };
 
-    $scope.clickThrough = angular.copy(obj);
-    $scope.shareRate = angular.copy(obj);
-    $scope.activations = angular.copy(obj);
+    $scope.impression = angular.copy(obj);
     $scope.engagement = angular.copy(obj);
+    $scope.chatter = angular.copy(obj);
+    $scope.shareRate = angular.copy(obj);
 
-    $scope.clickThrough.data = [100, 290, 320, 450];
-    $scope.shareRate.data = [100, 300, 350, 270];
-    $scope.activations.data = [100, 300, 350, 270];
-    $scope.engagement.data = [100, 300, 350, 270];
+    $scope.impression.data = [345, 450, 550, 753];
+    $scope.engagement.data = [50, 65, 95, 100];
+    $scope.chatter.data = [30, 50, 63, 60];
+    $scope.shareRate.data = [12, 19, 26, 37.5];
 });
